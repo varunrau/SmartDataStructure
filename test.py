30f8a71027a16c9c83831ddf5962c0e5c741044a
varunrau <varunrau@gmail.com>
2012-12-01 15:34:19 -0800

    added a very basic test file

---
+++ test.py
@@ -0,0 +1,14 @@
+# Test File
+
+s_data = new SD()
+
+for x in range(10):
+    s_data.add(x)
+
+print(str(s_data.contains(3)) + " should be true")
+
+s_data.remove(3)
+print(str(s_data.contains(3)) + " should be false")
+
+print(str(s_data.get(0)) + " should be 0")
+
