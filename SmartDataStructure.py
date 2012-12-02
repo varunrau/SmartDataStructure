8b574be258acb6b3b53a9d584720d7718e06ddc3
varunrau <varunrau@gmail.com>
2012-12-02 13:48:44 -0800

    fixed?

--- SmartDataStructure.py
+++ SmartDataStructure.py
@@ -164,7 +164,8 @@ class SD:
                 item = self.extract_max_new()
                 temp.add(item)
             else:
-                item = self.extract_min_new()
+                item = self.get(x)
+                self.remove(item)
                 temp.add(item)
         globalz.which = new
         self.struct = temp
