37ab9b6961580ecfd16cf225d9410db005242599
erks <erks@vbox.(none)>
2012-12-01 18:59:17 -0800

    Finished basic timing

--- CompileTimeTest.py
+++ CompileTimeTest.py
@@ -1,10 +1,28 @@
 #Compile Time Test
 from time import clock, time
+import fileinput, string
 
-def main():
-  print "hi"
+dataStructures = ["Arr()", "SArr()"]
+
+testFileName = "somefile"
+testFile = testFileName + ".py"
 
 
-start_time = time()
-main()
-print str(time() - start_time) + " seconds"
+
+for i in range(len(dataStructures)): #loop through dataStructures
+  newFileName = str(testFileName + "_" + dataStructures[i] + ".py")
+  file = open(newFileName, "w")
+
+  for line in fileinput.input(testFile): #replace SD() with a d.s.
+    line = line.replace("SD()", dataStructures[i]);
+    file.write(line)
+
+  start_time = time()
+  print "New file name: %s" % newFileName
+  execfile(str(newFileName));
+  print str(time() - start_time) + " " + "seconds\n"
+
+  file.close
+
+
+
