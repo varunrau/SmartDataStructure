7fc17e628cef80e0d1d686a9f0e09afb9e47aca1
varunrau <varunrau@gmail.com>
2012-12-01 23:17:29 -0800

    runtime swapping might work

--- SmartDataStructure.py
+++ SmartDataStructure.py
@@ -9,6 +9,8 @@ from bst import *
 from minheap import *
 from maxheap import *
 
+TIME_TO_REEVAL = 100
+
 class SD:
 
     """
@@ -37,6 +39,7 @@ class SD:
             print "Default to Array"
             self.struct = Arr()
 
+        self.which = which
         self.contains_ctr = 0
         self.add_ctr = 0
         self.remove_ctr = 0
@@ -46,12 +49,15 @@ class SD:
         self.extract_min_ctr = 0
         self.num_ops = 0
 
+        self.is_peak = True
+
         self.isTesting = isTesting
         self.len = 0
 
     def contains(self, key):
         self.contains_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
         return self.struct.contains(key)
 
     def add(self, key):
@@ -59,26 +65,31 @@ class SD:
         self.len += 1
         self.add_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
 
     def remove(self, key):
         self.struct.remove(key)
         self.len -= 1
         self.num_ops += 1
         self.remove_ctr += 1
+        self.time_to_reeval()
 
     def get(self, index):
         self.get_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
         return self.struct.get(index)
 
     def get_min(self):
         self.get_min_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
         return self.get(self, 0)
 
     def get_max(self):
         self.get_max_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
         return self.get(self, len - 1)
 
     def extract(self, index):
@@ -86,17 +97,20 @@ class SD:
         remove(self, index)
         self.extract_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
         return toReturn
 
     def extract_min(self):
         extract(self, 0)
         self.extract_min_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
 
     def extract_max(self):
         extract(self, self.len - 1)
         self.extract_max_ctr += 1
         self.num_ops += 1
+        self.time_to_reeval()
 
     def size(self):
         return self.len
@@ -105,6 +119,54 @@ class SD:
         if self.isTesting:
             return
         else:
-            print "NEED TO IMPLEMENT"
+            new_data_structure = self.best_datastructure()
+            if new_data_structure != self.which:
+                self.use_new_datastructure(new_data_structure)
 
+    def use_new_datastructure(self, new):
+        if new == DS.ARRAY:
+            temp = Arr()
+        elif new == DS.SORTED_ARRAY:
+            temp = SArr()
+        elif new == DS.MAX_HEAP:
+            temp = MaxHeap()
+        elif new == DS.MIN_HEAP:
+            temp = MinHeap()
+        elif new == DS.BINARY_SEARCH_TREE:
+            temp = Balanced_BST()
+        else:
+            temp = Arr()
+        for x in range(self.size()):
+            if which == DS.MAX_HEAP:
+                item = self.extract_max()
+                temp.add(item)
+            else:
+                item = self.extract(x)
+                temp.add(item)
+        self.struct = temp
+
+    def best_datastructure(self)
+        s = self.size()
+        log = math.log(s, 2)
+        arr = add_ctr + s * contains_ctr + s * remove_ctr + s * get_ctr
+        sarr = log * contains_ctr + s * add_ctr + remove_ctr * s + s
+        bst = log * contains_ctr + log * add_ctr + remove_ctr * log + get_ctr * log
+        htable = 3 * s + s * get_ctr
+        heap = contains_ctr * s + log * add_ctr + log * remove_ctr + log * get_ctr
+        array = [arr, sarr, bst, htable, heap]
+        mini = min(array)
+        counter = 0
+        smallest = array[0]
+        if mini < val * .75:
+            for val in array:
+                if mini == val:
+                    smallest = counter
+                counter += 1
+            return counter
+        return self.which
+
+
+    def time_to_reeval(self):
+        if self.num_ops >= TIME_TO_REEVAL:
+            self.reeval()
 
