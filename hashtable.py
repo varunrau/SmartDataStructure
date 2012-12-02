64e5949b338b97fd0a96f058b70b7df6516380c0
varunrau <varunrau@gmail.com>
2012-12-02 02:07:32 -0800

    implemented the hashtable

--- hashtable.py
+++ hashtable.py
@@ -1,146 +1,47 @@
 # Implementation of a HashTable
 # A wrapper of a Python Dictionary
 
-class Dict(object):
-    """
-    A mapping interface implemented as a hash table.
+def HashTable:
 
-    Attributes:
-        * used - The number of entires used in the table.
-        * filled - used + number of entries with a dummy key.
-        * table - List of entries; contains the actual dict data.
-        * mask - Length of table - 1. Used to fetch values.
-    """
+    def __init__(self, loadFactor = 10):
+        self.numBuckets = 10
+        self.buckets = []
+        for x in range(self.numBuckets):
+            self.buckets[] = [0]
+        self.loadFactor = loadFactor
+        self.numKeys = 0
 
-    __slots__ = ("filled", "used", "mask", "table")
-
-
-    def __init__(self, arg=None, **kwargs):
-        self.clear()
-        self._update(arg, kwargs)
-
-    @classmethod
-    def fromkeys(cls, keys, value=0):
-        """
-        Return a new dictionary from a sequence of keys.
-        """
-        d = cls()
-        for key in keys:
-            d[key] = value
-        return d
-
-    def pop(self, *args):
-        """
-        Remove and return the value for a key.
-        """
-        have_default = len(args) == 2
-        try:
-            v = self[args[0]]
-        except KeyError:
-            if have_default:
-                return args[1]
-            raise
-        else:
-            del self[args[0]]
-            return v
-
-    def setdefault(self, key, default=0):
-        """
-        If key is in the dictionary, return it. Otherwise, set it to the default
-        value.
-        """
-        val = self._lookup(key).value
-        if val is None:
-            self[key] = default
-            return default
-        return val
-
-    def _lookup(self, key):
-        """
-        Find the entry for a key.
-        """
+    def add(self, key):
+        self.numKeys += 1
+        if self.numKeys / self.numBuckets > self.loadFactor:
+            self.resize(self.numBuckets * 2)
         key_hash = hash(key)
-        entry = self.table[i]
-        if entry.key is None or entry is key:
-            return entry
-        free = None
-        if entry.key is dummy:
-            free = entry
-        elif entry.hash == key_hash and key == entry.key:
-            return entry
+        buckets[key_hash % numBuckets].append(key_hash)
 
-        perturb = key_hash
-        while True:
-            i = (i << 2) + i + perturb + 1;
-            entry = self.table[i & self.mask]
-            if entry.key is None:
-                return entry if free is None else free
-            if entry.key is key or \
-                    (entry.hash == key_hash and key == entry.key):
-                return entry
-            elif entry.key is dummy and free is None:
-                free = dummy
-            perturb >>= PERTURB_SHIFT
+    def resize(self, newSize):
+        self.numBuckets = newSize
+        numKeys = 0
+        oldBuckets = self.buckets
+        for x in range(newSize):
+            buckets.append([0])
+        for bucket in oldBuckets:
+            for key in bucket:
+                self.add(key)
 
-        assert False, "not reached"
+    def contains(self, key):
+        key_hash = hash(key)
+        for x in bucket[key_hash % self.numBuckets]:
+            if x == key:
+                return True
+        return False
 
-    def _insert(self, key, value):
-        """
-        Add a new value to the dictionary or replace an old one.
-        """
-        entry = self._lookup(key)
-        if entry.value is None:
-            self.used += 1
-            if entry.key is not dummy:
-                self.filled += 1
-        entry.key = key
-        entry.hash = hash(key)
-        entry.value = value
+    def remove(self, key):
+        self.numKeys -= 1
+        key_hash = hash(key)
+        buckets[key_hash % self.numBuckets].remove(key)
 
-    def _del(self, entry):
-        """
-        Mark an entry as free with the dummy key.
-        """
-        entry.key = dummy
-        entry.value = None
-        self.used -= 1
-
-    def __getitem__(self, key):
-        value = self._lookup(key).value
-        if value is None:
-            # Check if we're a subclass.
-            if type(self) is not Dict:
-                # Try to call the __missing__ method.
-                missing = getattr(self, "__missing__")
-                if missing is not None:
-                    return missing(key)
-            raise KeyError("no such key: {0!r}".format(key))
-        return value
-
-    def __setitem__(self, key, what):
-        # None is used as a marker for empty entries, so it can't be in a
-        # dictionary.
-        assert what is not None and key is not None, \
-            "key and value must not be None"
-        old_used = self.used
-        self._insert(key, what)
-        # Maybe resize the dict.
-        if not (self.used > old_used and
-                self.filled*3 >= (self.mask + 1)*2):
-            return
-        # Large dictionaries (< 5000) are only doubled in size.
-        factor = 2 if self.used > 5000 else 4
-        self._resize(factor*self.used)
-
-    def __delitem__(self, key):
-        entry = self._lookup(key)
-        if entry.value is None:
-            raise KeyError("no such key: {0!r}".format(key))
-        self._del(entry)
-
-    def __contains__(self, key):
-        """
-        Check if a key is in the dictionary.
-        """
-        return self._lookup(key).value is not None
-
+    def get(self, index):
+        full_list = []
+        for bucket in self.buckets:
+            full_list.append(bucket)
+        return full_list.sort().index(index)
