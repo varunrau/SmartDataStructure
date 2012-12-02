faf25ecacc02a04ad9b4481890d8336239227565
varunrau <varunrau@gmail.com>
2012-12-02 14:11:05 -0800

    fajdsk;fadsfjk;

--- hashtable.py
+++ hashtable.py
@@ -46,4 +46,4 @@ class Hashtable:
             if bucket is not None:
                 full_list.append(bucket)
 
-        return full_list.sort()[full_list.sort() is not None].index(index)
+        return full_list.sort().index(index)
