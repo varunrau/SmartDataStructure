26fe62bb143b2cc369a3fde2a8c479dc0f201828
varunrau <varunrau@gmail.com>
2012-12-01 17:05:17 -0800

    finished (un)sorted array

---
+++ arr.py
@@ -0,0 +1,43 @@
+import bisect
+
+class Arr:
+
+    """
+    Creates an array.
+    """
+    def __init__(self):
+        self.array = []
+
+    """
+    True iff the item is in the array.
+    """
+    def contains(self, key):
+        for x in self.array:
+            if x == key:
+                return True
+        return False
+
+    """
+    Adds the item to the array.
+    """
+    def add(self, key):
+        self.array.append(key)
+
+    """
+    Removes the item from the array.
+    """
+    def remove(self, key):
+        self.array.remove(key)
+
+    """
+    Returns the item at index index
+    """
+    def get(self, index):
+        return self.sort().index(index)
+
+    def sort(self):
+        l = []
+        for i in range(len(self.array)):
+            bisect.insort(l, self.array[i])
+        return l
+
